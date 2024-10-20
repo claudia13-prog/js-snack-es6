a867246a98011ba6c147c5655d8c424ae0caaea0
claudia13-prog <cappellaclaudia13@gmail.com>
2024-10-20 16:20:18 +0200

    assign values ​​to "punti" and "falli" properties

--- snack-2/script.js
+++ snack-2/script.js
@@ -4,37 +4,48 @@
 
 const teams = [{
     nome: 'Squadra 1',
-    'punti-fatti': 0,
-    'falli-subiti': 0
+    punti: 0,
+    falli: 0
 }, {
     nome: 'Squadra 2',
-    'punti-fatti': 0,
-    'falli-subiti': 0
+    punti: 0,
+    falli: 0
 }, {
     nome: 'Squadra 3',
-    'punti-fatti': 0,
-    'falli-subiti': 0
+    punti: 0,
+    falli: 0
 }, {
     nome: 'Squadra 4',
-    'punti-fatti': 0,
-    'falli-subiti': 0
+    punti: 0,
+    falli: 0
 }, {
     nome: 'Squadra 5',
-    'punti-fatti': 0,
-    'falli-subiti': 0
+    punti: 0,
+    falli: 0
 },{
     nome: 'Squadra 6',
-    'punti-fatti': 0,
-    'falli-subiti': 0
+    punti: 0,
+    falli: 0
 }];
 
-console.log(teams);
+console.log(teams[0].punti);
 
 /* Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 1) Creare una funzione che genera nunmeri random
-2) Richiamare la funzione in un ciclo 'for' che assegnerà i valori alle proprietà “punti” fatti e “falli subiti”
+2) Richiamare la funzione in un ciclo 'for' che assegnerà i valori (da 0 a 100) alle proprietà “punti” fatti e “falli subiti”
 */
 
+const teamsLenght = teams.length;
+
+for(let i = 0; i < teamsLenght; i++){
+
+    teams[i].punti = getRndInteger(0, 100);
+    teams[i].falli = getRndInteger(0, 100);
+
+
+}
+
+console.log(teams)
 
 
 /* Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
