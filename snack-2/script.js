f1bef8de42b178e327d8db9e7cc1d59b7aa8797f
claudia13-prog <cappellaclaudia13@gmail.com>
2024-10-20 15:51:00 +0200

    analysis problem

---
+++ snack-2/script.js
@@ -0,0 +1,10 @@
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+
+/* Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+1) Creare una funzione che genera nunmeri random
+2) Richiamare la funzione in un ciclo 'for' che assegnerà i valori alle proprietà “punti” fatti e “falli subiti”
+
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+1) Sfruttando il ciclo 'for' precedente creiamo un nuovo array con tutte le squadre senza la proprietà "punti fatti"
