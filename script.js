6782628da7506c6990c8572b425a8dcce433ae7f
claudia13-prog <cappellaclaudia13@gmail.com>
2024-10-20 10:54:09 +0200

    defined a "bike" array

---
+++ script.js
@@ -0,0 +1,26 @@
+/* Creare un array di oggetti:
+ Ogni oggetto descriverà una bici da corsa
+con le seguenti proprietà: nome e peso.
+
+*/
+
+const bike = [{
+    nome: 'Bike 1',
+    peso: 5
+}, {
+    nome: 'Bike 2',
+    peso: 4
+}, {
+    nome: 'Bike 3',
+    peso: 6
+}, {
+    nome: 'Bike 4',
+    peso: 7
+}, {
+    nome: 'Bike 5',
+    peso: 3
+}];
+
+console.log(bike);
+
+//Stampare a schermo la bici con peso minore.
