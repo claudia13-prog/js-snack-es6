b5397973235672cf563e305d8e7025e678b047bd
claudia13-prog <cappellaclaudia13@gmail.com>
2024-10-20 16:00:42 +0200

    created "teams" array

--- snack-2/script.js
+++ snack-2/script.js
@@ -2,9 +2,39 @@
 // Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 
+const teams = [{
+    nome: 'Squadra 1',
+    'punti-fatti': 0,
+    'falli-subiti': 0
+}, {
+    nome: 'Squadra 2',
+    'punti-fatti': 0,
+    'falli-subiti': 0
+}, {
+    nome: 'Squadra 3',
+    'punti-fatti': 0,
+    'falli-subiti': 0
+}, {
+    nome: 'Squadra 4',
+    'punti-fatti': 0,
+    'falli-subiti': 0
+}, {
+    nome: 'Squadra 5',
+    'punti-fatti': 0,
+    'falli-subiti': 0
+},{
+    nome: 'Squadra 6',
+    'punti-fatti': 0,
+    'falli-subiti': 0
+}];
+
+console.log(teams);
+
 /* Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 1) Creare una funzione che genera nunmeri random
 2) Richiamare la funzione in un ciclo 'for' che assegnerà i valori alle proprietà “punti” fatti e “falli subiti”
+*/
 
-// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+/* Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 1) Sfruttando il ciclo 'for' precedente creiamo un nuovo array con tutte le squadre senza la proprietà "punti fatti"
+*/
