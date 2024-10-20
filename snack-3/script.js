16b11c46c5053e107c7be2c4368f14cf018b4811
claudia13-prog <cappellaclaudia13@gmail.com>
2024-10-20 18:49:57 +0200

    created function fromAToBNumbers

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,2 +1,38 @@
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+
+ const result = fromAToBNumbers(15, 10);
+
+console.log(result);
+
+/*
+* @param {number} a
+* @param {number} b
+* @returns {array}
+*/
+function fromAToBNumbers(a, b){
+
+    const array = [];
+
+    if(a <= b) {
+
+        for(let i = a; i <= b; i++){
+
+            array.push(i);
+
+        }
+    } else {
+
+       return console.log('Errore: a > b');
+
+    }
+
+
+
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+
+    return array;
+
+
+}
+
+
